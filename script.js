38bf42422878a8f5784768dd19bd7e59e8f8f5f0
Ha Viet Giang <gianghvse05576@fpt.edu.vn>
2024-05-20 23:51:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('div').innerHTML = 'Okayy'
+// get được element
+// Set được thuộc tính cho element
+// xóa -> thêm element,...
+
+// React
+// React quan tâm đến dữ liệu, việc render sẽ được react handle thay.
